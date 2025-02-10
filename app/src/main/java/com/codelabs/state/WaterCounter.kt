b9d4314c0a1ef2389497afd90704344ae1028427
Rafael Herdani <147459978+RafaelHerdani@users.noreply.github.com>
2025-02-11 06:11:31 +0700

    state hoisting

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -48,28 +48,26 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun StatefulCounter(modifier: Modifier = Modifier) {
-    var count by rememberSaveable { mutableStateOf(0) }
-    StatelessCounter(
-        count = count,
-        onIncrement = { count++ },
-        modifier = modifier
-    )
-}
-
-@Composable
-fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        if (count > 0) {
-            Text("You've had $count glasses.")
-        }
-        Button(
-            onClick = onIncrement,
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Add one")
+    @Composable
+    fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+        Column(modifier = modifier.padding(16.dp)) {
+            if (count > 0) {
+                Text("You've had $count glasses.")
+            }
+            Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+                Text("Add one")
+            }
         }
     }
-}
+
+    @Composable
+    fun StatefulCounter() {
+        var waterCount by remember { mutableStateOf(0) }
+
+        var juiceCount by remember { mutableStateOf(0) }
+
+        StatelessCounter(waterCount, { waterCount++ })
+        StatelessCounter(juiceCount, { juiceCount++ })
+    }
+
+
