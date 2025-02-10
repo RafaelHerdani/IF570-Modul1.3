7498795a606b63dcf278b74727474f0c0473b843
Rafael Herdani <147459978+RafaelHerdani@users.noreply.github.com>
2025-02-11 06:07:58 +0700

    Restore state in Compose

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -37,8 +37,7 @@ import androidx.compose.runtime.setValue
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("You've had $count glasses.")
